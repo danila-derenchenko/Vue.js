7e7576deac9b0105afa95596fc8bf631c70ccd49
Danila <danila.derenchenko@gmail.com>
2021-07-13 18:24:57 +0400

    Add homework

---
+++ src/components/PaymentsDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="payments-list">
+    <div># - data - category - value</div>
+    <div v-for="(item, idx) in list" :key="idx">
+      {{ idx + 1 }}) {{ item.date }} - {{ item.category }} - {{ item.value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
